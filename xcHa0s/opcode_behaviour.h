880dc93bcaa75818f6d79dc6cb4ae761fae75d9a
adrianpop3 <adrian.pop2@student.upt.ro>
2022-12-14 00:55:05 +0200

    Increment, Decrement, Compare & Test added.

--- xcHa0s/opcode_behaviour.h
+++ xcHa0s/opcode_behaviour.h
@@ -23,15 +23,18 @@ void analize_arguments (char line[]) {
 }
 
 uint16_t execute_nop (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 0) {
         printf("Error at line %d! NOP doesn't take any parameters!\n", nr_line);
         fail_exit();
     }
+
     return 0x0000;
 }
 
 uint16_t execute_psh (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 1) {
         printf("Error at line %d! PSH takes 1 parameter!\n", nr_line);
@@ -55,6 +58,7 @@ uint16_t execute_psh (char line[4096], int nr_line) {
 }
 
 uint16_t execute_pop (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 1) {
         printf("Error at line %d! POP takes 1 parameter!\n", nr_line);
@@ -78,6 +82,7 @@ uint16_t execute_pop (char line[4096], int nr_line) {
 }
 
 uint16_t execute_mov (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 2) {
         printf("Error at line %d! MOV takes 2 parameters!\n", nr_line);
@@ -122,6 +127,25 @@ uint16_t execute_mov (char line[4096], int nr_line) {
         }
     }
 
+    if(strcmp(params[0], "ACC") == 0) {
+        if(strcmp(params[1], "X") == 0)
+            return 0b0001111000000000;
+        else if(strcmp(params[1], "Y") == 0)
+            return 0b0001111001000000;
+        else{
+            int imm;
+            if(sscanf(params[1], "%d", &imm) == 0){
+                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+                fail_exit();
+            }
+            if(imm < -256 || imm > 255){
+                printf("Error at line %d! Immediate out of range.\n", nr_line);
+                fail_exit();
+            }
+            return (0b0001110000000000 | (imm & 0b0000000111111111));
+        }
+    }
+
     else {
         printf("Error at line %d! Bad parameter %s !\n", nr_line, params[0]);
         fail_exit();
@@ -130,15 +154,18 @@ uint16_t execute_mov (char line[4096], int nr_line) {
 }
 
 uint16_t execute_rad (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 0) {
         printf("Error at line %d! RAD doesn't take any parameters!\n", nr_line);
         fail_exit();
     }
+
     return 0b0010000000000000;
 }
 
 uint16_t execute_pow (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 1) {
         printf("Error at line %d! RAD takes 1 parameter!\n", nr_line);
@@ -162,6 +189,7 @@ uint16_t execute_pow (char line[4096], int nr_line) {
 }
 
 uint16_t execute_add (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 2) {
         printf("Error at line %d! ADD takes 2 parameters!\n", nr_line);
@@ -235,6 +263,7 @@ uint16_t execute_add (char line[4096], int nr_line) {
 }
 
 uint16_t execute_sub (char line[4096], int nr_line) {
+
     analize_arguments(line);
     if(nr_params != 2) {
         printf("Error at line %d! SUB takes 2 parameters!\n", nr_line);
@@ -312,6 +341,7 @@ uint16_t execute_MulDivModAndOrXor (char line[4096], int nr_line) {
     int data;
     char word[256];
     sscanf(line, "%s", word);
+
     analize_arguments(line);
     if(nr_params != 2) {
         printf("Error at line %d! %s takes 2 parameters!\n", nr_line, word);
@@ -391,8 +421,8 @@ uint16_t execute_NotNeg (char line[4096], int nr_line) {
     int data;
     char word[256];
     sscanf(line, "%s", word);
+
     analize_arguments(line);
-    
     if(nr_params != 1) {
         printf("Error at line %d! %s takes 1 parameter!\n", nr_line, word);
         fail_exit();
@@ -421,6 +451,194 @@ uint16_t execute_NotNeg (char line[4096], int nr_line) {
     return ((opcode<<10) | (data<<8));
 }
 
+uint16_t execute_inc(char line[4096], int nr_line) {
+
+    analize_arguments(line);
+    if (nr_params != 1) {
+        printf("Error at line %d! INC takes 1 parameter!\n", nr_line);
+        fail_exit();
+    }
+
+    if (strcmp(params[0], "X") == 0) {
+        return 0b1010100000000000;
+    } else if (strcmp(params[0], "Y") == 0) {
+        return 0b1010100100000000;
+    } else if (strcmp(params[0], "ACC") == 0) {
+        return 0b1010101000000000;
+    } else {
+        printf("Error at line %d! Bad parameter %s !\n", nr_line, params[0]);
+        fail_exit();
+    }
+
+    return 1;
+}
+
+uint16_t execute_dec(char line[4096], int nr_line) {
+
+    analize_arguments(line);
+    if (nr_params != 1) {
+        printf("Error at line %d! DEC takes 1 parameter!\n", nr_line);
+        fail_exit();
+    }
+
+    if (strcmp(params[0], "X") == 0) {
+        return 0b1010110000000000;
+    } else if (strcmp(params[0], "Y") == 0) {
+        return 0b1010110100000000;
+    } else if (strcmp(params[0], "ACC") == 0) {
+        return 0b1010111000000000;
+    } else {
+        printf("Error at line %d! Bad parameter %s !\n", nr_line, params[0]);
+        fail_exit();
+    }
+
+    return 1;
+}
+
+uint16_t execute_cmp(char line[4096], int nr_line) {
+
+    analize_arguments(line);
+    if (nr_params != 2) {
+        printf("Error at line %d! CMP takes 2 parameters!\n", nr_line);
+        fail_exit();
+    }
+
+    if(strcmp(params[0], "X") == 0) {
+        if(strcmp(params[1], "Y") == 0)
+            return 0b1111000001000000;
+        else if(strcmp(params[1], "ACC") == 0)
+            return 0b1111000010000000;
+        else{
+            int imm;
+            if(sscanf(params[1], "%d", &imm) == 0){
+                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+                fail_exit();
+            }
+            if(imm < -256 || imm > 255){
+                printf("Error at line %d! Immediate out of range.\n", nr_line);
+                fail_exit();
+            }
+            return (0b1110100000000000 | (imm & 0b0000000111111111));
+        }
+    }
+
+    if(strcmp(params[0], "Y") == 0) {
+        if(strcmp(params[1], "X") == 0)
+            return 0b1111000100000000;
+        else if(strcmp(params[1], "ACC") == 0)
+            return 0b1111000110000000;
+        else{
+            int imm;
+            if(sscanf(params[1], "%d", &imm) == 0){
+                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+                fail_exit();
+            }
+            if(imm < -256 || imm > 255){
+                printf("Error at line %d! Immediate out of range.\n", nr_line);
+                fail_exit();
+            }
+            return (0b1110101000000000 | (imm & 0b0000000111111111));
+        }
+    }
+
+    if(strcmp(params[0], "ACC") == 0) {
+        if(strcmp(params[1], "X") == 0)
+            return 0b1111001000000000;
+        else if(strcmp(params[1], "Y") == 0)
+            return 0b1111001001000000;
+        else{
+            int imm;
+            if(sscanf(params[1], "%d", &imm) == 0){
+                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+                fail_exit();
+            }
+            if(imm < -256 || imm > 255){
+                printf("Error at line %d! Immediate out of range.\n", nr_line);
+                fail_exit();
+            }
+            return (0b1110110000000000 | (imm & 0b0000000111111111));
+        }
+    }
+
+    else {
+        printf("Error at line %d! Bad parameter %s !\n", nr_line, params[0]);
+        fail_exit();
+    }
+    return 1;
+}
+
+uint16_t execute_tst(char line[4096], int nr_line) {
+
+    analize_arguments(line);
+    if (nr_params != 2) {
+        printf("Error at line %d! TST takes 2 parameters!\n", nr_line);
+        fail_exit();
+    }
+
+    if(strcmp(params[0], "X") == 0) {
+        if(strcmp(params[1], "Y") == 0)
+            return 0b1111110001000000;
+        else if(strcmp(params[1], "ACC") == 0)
+            return 0b1111110010000000;
+        else{
+            int imm;
+            if(sscanf(params[1], "%d", &imm) == 0){
+                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+                fail_exit();
+            }
+            if(imm < -256 || imm > 255){
+                printf("Error at line %d! Immediate out of range.\n", nr_line);
+                fail_exit();
+            }
+            return (0b1111010000000000 | (imm & 0b0000000111111111));
+        }
+    }
+
+    if(strcmp(params[0], "Y") == 0) {
+        if(strcmp(params[1], "X") == 0)
+            return 0b1111110100000000;
+        else if(strcmp(params[1], "ACC") == 0)
+            return 0b1111110110000000;
+        else{
+            int imm;
+            if(sscanf(params[1], "%d", &imm) == 0){
+                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+                fail_exit();
+            }
+            if(imm < -256 || imm > 255){
+                printf("Error at line %d! Immediate out of range.\n", nr_line);
+                fail_exit();
+            }
+            return (0b1111011000000000 | (imm & 0b0000000111111111));
+        }
+    }
+
+    if(strcmp(params[0], "ACC") == 0) {
+        if(strcmp(params[1], "X") == 0)
+            return 0b1111111000000000;
+        else if(strcmp(params[1], "Y") == 0)
+            return 0b1111111001000000;
+        else{
+            int imm;
+            if(sscanf(params[1], "%d", &imm) == 0){
+                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+                fail_exit();
+            }
+            if(imm < -256 || imm > 255){
+                printf("Error at line %d! Immediate out of range.\n", nr_line);
+                fail_exit();
+            }
+            return (0b1111100000000000 | (imm & 0b0000000111111111));
+        }
+    }
+
+    else {
+        printf("Error at line %d! Bad parameter %s !\n", nr_line, params[0]);
+        fail_exit();
+    }
+    return 1;
+}
+
 instruction instructions[64] = {
     {"NOP", execute_nop},
     {"PSH", execute_psh},
@@ -438,4 +656,8 @@ instruction instructions[64] = {
     {"XOR", execute_MulDivModAndOrXor},
     {"NOT", execute_NotNeg},
     {"NEG", execute_NotNeg},
+    {"INC", execute_inc},
+    {"DEC", execute_dec},
+    {"CMP", execute_cmp},
+    {"TST", execute_tst},
 };
