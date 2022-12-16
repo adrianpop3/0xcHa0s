806e66915491b883d02c91a0970ac626d108ce65
vlad-plavat <vlad.plavat@student.upt.ro>
2022-12-16 02:13:02 +0200

    finished assembler

--- xcHa0s/assembler.c
+++ xcHa0s/assembler.c
@@ -2,9 +2,24 @@
 #include<stdio.h>
 #include<stdint.h>
 #include<string.h>
+#include<ctype.h>
 
 uint16_t result[1024];
 int nr_op = 0;
+
+typedef struct{
+	char name[256];
+	int address;
+}variable;
+variable variables [512];
+int nr_variables,cr_data_addr = 0;
+
+typedef struct{
+    char name[256];
+    int address;
+}label;
+label labels [1024];
+int nr_labels;
 
 FILE *read_file, *write_file;
 
@@ -24,14 +39,19 @@ void print_binary(uint16_t bin) {
     fprintf(write_file, "\n");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     char line[4096];
     int nr_lines=0; 
 
-    read_file = fopen("main.asm", "rt");
+    if(argc < 2){
+        read_file = fopen("main.asm", "rt");
+    }else{
+        read_file = fopen(argv[1], "rt");
+    }
     if(read_file == NULL) {
         printf("Eroare la deschiderea fisierului!");
+        return 1;
     }
 
     write_file = fopen("main.bin", "wt");
@@ -40,24 +60,88 @@ int main()
     }
 
     while(fgets(line, 4096, read_file)) {
+        nr_lines++;
+        char *s = line;
+        while (*s) {*s = toupper((unsigned char) *s);    s++;}
+        while(isspace(*line) && *line){
+            s=line;
+            while(*s){*s=*(s+1);s++;}
+        }
+        
+        
         if(line[0] < 'A' || line[0] > 'Z') {
             continue;
         }
         line[strcspn(line,"\n")] = '\0';
 
-        nr_lines++; 
         char word[256];
         sscanf(line, "%s", word);
 
         int found_instr = 0; 
+		
+		if(strchr(word,' ')==NULL && strchr(word,'\t')==NULL && strchr(word,'\r')==NULL){
+		    //no spaces
+		    if(word[strlen(word)-1] == ':'){
+		        //LABEL:
+		        word[strlen(word)-1]='\0';
+    		    for(int i=0; i<nr_labels; i++){
+    			    if(strcmp(labels[i].name,word)==0){
+                        printf("Error at line %d! Label already declared.\n", nr_lines);
+                        fail_exit();
+    			    }
+    			}
+		        strcpy(labels[nr_labels].name,word);
+		        labels[nr_labels].address = nr_op;
+		        nr_labels++;
+		        continue;
+		    }
+		    
+		}
+		
+		if(strcmp(word, "DATA") == 0){
+			analize_arguments(line);
+			for(int i=0; i<nr_variables; i++){
+			    if(strcmp(variables[i].name,params[0])==0){
+                    printf("Error at line %d! Variable already declared.\n", nr_lines);
+                    fail_exit();
+			    }
+			}
+			
+			variables[nr_variables].address = cr_data_addr;
+			strcpy(variables[nr_variables].name,params[0]);
+			nr_variables++;
+			if(nr_params == 1){
+			    cr_data_addr++;
+			}else if(nr_params == 2){
+			    int imm;
+                if(sscanf(params[1], "%d", &imm) == 0 || imm < 1){
+                    printf("Error at line %d! Invalid vector size value.\n", nr_lines);
+                    fail_exit();
+                }
+                cr_data_addr += imm;
+			}else{
+			    printf("Error. Too many parameters for data declaration.\n");
+                fail_exit();
+			}
+			if(cr_data_addr > 512){
+                printf("Error. Too many variables.\n");
+                fail_exit();
+			}
+			continue;
+		}
+		
         for(int i=0; ;i++) {
             if(instructions[i].decode_instruction == NULL) {
                 break;
             }
-
+			
             if(strcmp(instructions[i].name, word) == 0) {
                 found_instr = 1;
                 uint16_t opcode = instructions[i].decode_instruction(line, nr_lines);
+                if(nr_op >= 1024){
+                    printf("Error! The code doesn't fit in ROM.\n");
+                    fail_exit();
+                }
                 result[nr_op++] = opcode;
                 break;
             }
@@ -73,6 +157,18 @@ int main()
     for(i=0; i<1024; i++) {
         print_binary(result[i]);
     }
+    
+    
+	FILE *mapfile = fopen("mem_map.txt","wt");
+	if(mapfile != NULL){
+    	for(i=0;i<nr_variables;i++){
+    		fprintf(mapfile,"%15s @ %d\n",variables[i].name,variables[i].address);
+    	}
+    	fprintf(mapfile,"\n");
+    	for(i=0;i<nr_labels;i++){
+    		fprintf(mapfile,"%15s & %d\n",labels[i].name,labels[i].address);
+    	}
+	}
 
     fclose(read_file);
     fclose(write_file);
--- xcHa0s/opcode_behaviour.h
+++ xcHa0s/opcode_behaviour.h
@@ -91,9 +91,9 @@ uint16_t execute_mov (char line[4096], int nr_line) {
 
     if(strcmp(params[0], "X") == 0) {
         if(strcmp(params[1], "Y") == 0)
-            return 0b0001110000000000;
+            return 0b0001110001000000;
         else if(strcmp(params[1], "ACC") == 0)
-            return 0b0001110100000000;
+            return 0b0001110010000000;
         else{
             int imm;
             if(sscanf(params[1], "%d", &imm) == 0){
@@ -110,9 +110,9 @@ uint16_t execute_mov (char line[4096], int nr_line) {
     
     if(strcmp(params[0], "Y") == 0) {
         if(strcmp(params[1], "X") == 0)
-            return 0b0001111000000000;
+            return 0b0001110100000000;
         else if(strcmp(params[1], "ACC") == 0)
-            return 0b0001111100000000;
+            return 0b0001110110000000;
         else{
             int imm;
             if(sscanf(params[1], "%d", &imm) == 0){
@@ -133,16 +133,8 @@ uint16_t execute_mov (char line[4096], int nr_line) {
         else if(strcmp(params[1], "Y") == 0)
             return 0b0001111001000000;
         else{
-            int imm;
-            if(sscanf(params[1], "%d", &imm) == 0){
-                printf("Error at line %d! Invalid immediate or register.\n", nr_line);
-                fail_exit();
-            }
-            if(imm < -256 || imm > 255){
-                printf("Error at line %d! Immediate out of range.\n", nr_line);
-                fail_exit();
-            }
-            return (0b0001110000000000 | (imm & 0b0000000111111111));
+            printf("Error at line %d! Bad parameter %s !\n", nr_line, params[0]);
+            fail_exit();
         }
     }
 
@@ -639,6 +631,247 @@ uint16_t execute_tst(char line[4096], int nr_line) {
     return 1;
 }
 
+uint16_t execute_load(char line[4096], int nr_line) {
+    analize_arguments(line);
+    if (nr_params != 2) {
+        printf("Error at line %d! LOAD takes 2 parameters!\n", nr_line);
+        fail_exit();
+    }
+
+    if(strcmp(params[0], "X") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0000100000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+
+    if(strcmp(params[0], "Y") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0000101000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+    return 1;
+}
+
+
+uint16_t execute_store(char line[4096], int nr_line) {
+    analize_arguments(line);
+    if (nr_params != 2) {
+        printf("Error at line %d! STORE takes 2 parameters!\n", nr_line);
+        fail_exit();
+    }
+
+    if(strcmp(params[0], "X") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0000110000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+
+    if(strcmp(params[0], "Y") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0000111000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+    return 1;
+}
+
+
+uint16_t execute_loadb(char line[4096], int nr_line) {
+    analize_arguments(line);
+    if (nr_params != 2) {
+        printf("Error at line %d! LOADB takes 2 parameters!\n", nr_line);
+        fail_exit();
+    }
+
+    if(strcmp(params[0], "X") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0001000000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+
+    if(strcmp(params[0], "Y") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0001001000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+    return 1;
+}
+
+
+uint16_t execute_storeb(char line[4096], int nr_line) {
+    analize_arguments(line);
+    if (nr_params != 2) {
+        printf("Error at line %d! STOREB takes 2 parameters!\n", nr_line);
+        fail_exit();
+    }
+
+    if(strcmp(params[0], "X") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0001010000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+
+    if(strcmp(params[0], "Y") == 0) {
+        int adr_target=-1;
+        for(int i=0; i<nr_variables; i++){
+            if(strcmp(params[1],variables[i].name) == 0){
+                adr_target = variables[i].address;
+                return 0b0001011000000000 | adr_target;
+            }
+        }
+        printf("Error at line %d! Undeclared variable '%s'.\n", nr_line,params[1]);
+        fail_exit();
+        return 1;
+    }
+    return 1;
+}
+
+
+uint16_t execute_shift(char line[4096], int nr_line) {
+    int oper = 0;
+    int cine = 0;
+    int iq   = 0;
+    char word[256];
+    sscanf(line, "%s", word);
+
+    analize_arguments(line);
+    if(nr_params != 2) {
+        printf("Error at line %d! %s takes 2 parameters!\n", nr_line, word);
+        fail_exit();
+    }
+    
+    if(word[0]=='L') oper |= 0b001;
+    if(word[2]=='L') oper |= 0b000;
+    if(word[2]=='A') oper |= 0b010;
+    if(word[2]=='R') oper |= 0b100;
+    if(word[2]=='C') oper |= 0b110;
+
+    if(strcmp(params[0], "X") == 0){
+        cine = 0b00;
+    }else if(strcmp(params[0], "Y") == 0){
+        cine = 0b01;
+    }else if(strcmp(params[0], "ACC") == 0){
+        cine = 0b10;
+    }else {
+        printf("Error at line %d! Bad parameter %s !\n", nr_line, params[0]);
+        fail_exit();
+    }
+    
+    //I QQQQ = 1 QQQQ = 0 AAMX
+    if(strcmp(params[1], "X") == 0){
+        iq = 0b00000;
+        if(strchr(word,'M')) iq |= 0b00010;
+    }else if(strcmp(params[1], "Y") == 0){
+        iq = 0b00100;
+        if(strchr(word,'M')) iq |= 0b00010;
+    }else if(strcmp(params[1], "ACC") == 0){
+        iq = 0b01000;
+        if(strchr(word,'M')) iq |= 0b00010;
+    }else {
+        int imm;
+        if(sscanf(params[1], "%d", &imm) == 0){
+            printf("Error at line %d! Invalid immediate or register.\n", nr_line);
+            fail_exit();
+        }
+        if(imm < 1 || imm > 15){
+            printf("Error at line %d! Immediate out of range.\n", nr_line);
+            fail_exit();
+        }
+        iq = 0b10000 | (imm & 0b1111);
+    }
+    return ((0b101000<<10) | (oper<<7) | (cine<<5) | iq);
+}
+
+uint16_t execute_branch (char line[4096], int nr_line) {
+    int opcode;
+    int data;
+    char word[256];
+    sscanf(line, "%s", word);
+
+    analize_arguments(line);
+    if(nr_params != 1) {
+        printf("Error at line %d! %s takes 1 parameter!\n", nr_line, word);
+        fail_exit();
+    }
+
+    if(strcmp(word, "BRA") == 0)        opcode = 0b010000;
+    else if(strcmp(word, "BRE") == 0)   opcode = 0b010001;
+    else if(strcmp(word, "BRZ") == 0)   opcode = 0b010001;
+    else if(strcmp(word, "BNE") == 0)   opcode = 0b010010;
+    else if(strcmp(word, "BRN") == 0)   opcode = 0b010011;
+    else if(strcmp(word, "BLT") == 0)   opcode = 0b010011;
+    else if(strcmp(word, "BRP") == 0)   opcode = 0b010100;
+    else if(strcmp(word, "BGT") == 0)   opcode = 0b010100;
+    
+    else if(strcmp(word, "BLE") == 0)   opcode = 0b010101;
+    else if(strcmp(word, "BGE") == 0)   opcode = 0b010110;
+    else if(strcmp(word, "BRC") == 0)   opcode = 0b010111;
+    else if(strcmp(word, "BRO") == 0)   opcode = 0b011000;
+    
+    else if(strcmp(word, "JMP") == 0)   opcode = 0b011110;
+    else if(strcmp(word, "RET") == 0)   opcode = 0b011111;
+    
+    int adr_target=-1;
+    for(int i=0; i<nr_labels; i++){
+        if(strcmp(params[0],labels[i].name) == 0){
+            adr_target = labels[i].address;
+            return ((opcode<<10) | adr_target);
+        }
+    }
+    printf("Error at line %d! Undeclared label '%s'.\n", nr_line,params[0]);
+    fail_exit();
+    return 1;
+}
+
+
 instruction instructions[64] = {
     {"NOP", execute_nop},
     {"PSH", execute_psh},
@@ -660,4 +893,43 @@ instruction instructions[64] = {
     {"DEC", execute_dec},
     {"CMP", execute_cmp},
     {"TST", execute_tst},
+    {"LOAD", execute_load},
+    {"STORE", execute_store},
+    {"LOADB", execute_loadb},
+    {"STOREB", execute_storeb},
+    {"STOREB", execute_storeb},
+    
+    {"RSLM", execute_shift},
+    {"LSLM", execute_shift},
+    {"RSAM", execute_shift},
+    {"LSAM", execute_shift},
+    {"RSRM", execute_shift},
+    {"LSRM", execute_shift},
+    {"RSCM", execute_shift},
+    {"LSCM", execute_shift},
+    
+    {"RSL", execute_shift},
+    {"LSL", execute_shift},
+    {"RSA", execute_shift},
+    {"LSA", execute_shift},
+    {"RSR", execute_shift},
+    {"LSR", execute_shift},
+    {"RSC", execute_shift},
+    {"LSC", execute_shift},
+    
+    {"BRA", execute_branch},
+    {"BRE", execute_branch},
+    {"BRZ", execute_branch},
+    {"BNE", execute_branch},
+    {"BRN", execute_branch},
+    {"BLT", execute_branch},
+    {"BRP", execute_branch},
+    {"BGT", execute_branch},
+    {"BLE", execute_branch},
+    {"BGE", execute_branch},
+    {"BRC", execute_branch},
+    {"BRO", execute_branch},
+    
+    {"JMP", execute_branch},
+    {"RET", execute_branch},
 };
