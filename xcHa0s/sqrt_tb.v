f7ae47e22fd64d5137ef3c6dce3e1d1609873b99
carlaploscaru <carla.ploscaru@student.upt.ro>
2023-01-05 21:37:29 +0200

    made tests for sqrt and pow

--- xcHa0s/sqrt_tb.v
+++ xcHa0s/sqrt_tb.v
@@ -30,7 +30,9 @@ module sqrt_tb;
 	initial forever begin
 		#10 clk = ~clk;
 	end
-
+  
+  reg ok;
+  reg [15:0]expected_sqrt;
 	initial begin
 		// Initialize Inputs
 		clk = 0;
@@ -42,13 +44,44 @@ module sqrt_tb;
 		#55;
 		rst = 0;
 		#40;
-		initial_nr = -64;
-		#20
+		//initial_nr = -64;
+		initial_nr=-9;
 		start=1;
-		#20;
-		while(ready == 0) #1;
-		#5
-		start=0;
+		  #20;
+		  while(ready == 0) #1;
+		  #5;
+		  if(Cflag==0)
+		    $display("error negative number should have set carry on 1");
+		    
+		    
+		initial_nr=(2**31)-1;
+		start=1;
+		  #20;
+		  while(ready == 0) #1;
+		  #5;
+		  if(Oflag==0)
+		    $display("error negative number should have set overflow on 1");
+		
+		
+		initial_nr=0;
+		ok=1;
+		
+		while(ok==1)begin
+		  start=1;
+		  #20;
+		  while(ready == 0) #1;
+		  #5;
+		  //$display("sqrt(%d)=%d",initial_nr,result);
+		  expected_sqrt=$floor($sqrt(initial_nr));
+		  if(expected_sqrt!=result)
+		  $display("error: sqrt(%d)expected %h got %h ",initial_nr,expected_sqrt,result);
+		  if(initial_nr==(2**31)-1)ok=0;
+		  initial_nr=initial_nr+1;
+		  if(initial_nr%1000==0)
+		    $display("Finished %d test",initial_nr);
+		    
+		end
+		 $display("finished all tests\n",);
         
 		// Add stimulus here
 
